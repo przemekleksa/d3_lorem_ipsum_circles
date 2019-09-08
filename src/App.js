beaaee49344c66112f315707340bcce2a8b5557a
Przemyslaw Leksa <38156110+gnoosny@users.noreply.github.com>
2019-09-08 13:30:52 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import * as d3 from 'd3';
 
-function App() {
+
+export default function App () {
+  const [count, setCount] = useState([1]);
+  useEffect(() => {
+     document.title =`${count.length-1}`;
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="buttons">
+          <button
+              onClick={() => {
+                setCount([...count, 1])
+                d3.select(".circles").selectAll("svg").data(count).enter().append("svg").attr("width", "200").attr("height", "200").append("g").append("circle").attr("cx", 100).attr("cy", 100).attr("r", 100).style("fill", "#03a9fc")
+                d3.selectAll("g").append("text").attr("x", "50%").attr("y", "50%").attr("font-size", "25px").attr("text-anchor", "middle").text("Lorem Ipsum");
+              }}
+              >
+              Dodaj nowe kółko
+          </button>
+          <button
+              onClick={() => {
+                if (count.length > 1) {
+                  setCount(count.splice(0, count.length-1))
+                }
+                
+                d3.select("svg").remove()
+              }}
+          >
+              Usuń ostatnie kółko
+          </button>
+        </div>
+        <div className="circles">
+              
+        </div>
+        
     </div>
-  );
-}
+  )
 
-export default App;
+}
